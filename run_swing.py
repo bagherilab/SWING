878576dcc25dec731e0883d9815b3a3b9eb9ec17
justinfinkle <doubledaffy888@gmail.com>
2018-02-07 17:45:20 -0600

    Add basic script for running swing

---
+++ run_swing.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from Swing import Swing
+from Swing.util.Evaluator import Evaluator
+
+# Load data
+gene_start_column = 1
+time_label = "Time"
+separator = "\t"
+gene_end = None
+file_path = "data/Ecoli_10_node/Ecoli10-1_dream4_timeseries.tsv"
+gold_standard_file = "data/Ecoli_10_node/Ecoli10-1_goldstandard.tsv"
+df = pd.read_csv(file_path, sep=separator)
+
+# Set SWING parameters
+k_min = 1
+k_max = 3
+w = 10
+method = 'RandomForest'
+trees = 100
+
+# Initialize a SWING object
+sg = Swing(file_path, gene_start_column, gene_end, time_label, separator, min_lag=k_min,
+           max_lag=k_max, window_width=w, window_type=method)
+
+sg.zscore_all_data()
+sg.create_windows()
+sg.optimize_params()
+
+sg.fit_windows(n_trees=trees, show_progress=False, n_jobs=-1)
+sg.rank_edges(permutation_n=10)
+sg.compile_roller_edges(self_edges=False)
+
+
+# Load the reference standard for scoring in silico predictions
+evaluator = Evaluator(gold_standard_file, '\t')
+true_edges = evaluator.gs_flat.tolist()
+
+sg.make_static_edge_dict(true_edges, self_edges=False, lag_method='mean_mean')
+ranked_edges = sg.make_sort_df(sg.edge_dict)
+
+roc_dict, pr_dict = sg.score(ranked_edges, gold_standard_file=gold_standard_file)
+print(roc_dict['auroc'][-1])
