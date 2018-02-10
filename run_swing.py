e855de5cccdcc404d6b9e5ca0486f4ba3b46177a
Jia Jie Wu <jia@Jias-MacBook-Pro.local>
2018-02-10 10:41:15 -0800

    slightly updated run_swing.py file

--- run_swing.py
+++ run_swing.py
@@ -1,35 +1,38 @@
 import pandas as pd
 from Swing import Swing
-# Load data
-gene_start_column = 1
-time_label = "Time"
-separator = "\t"
-gene_end = None
-file_path = "data/Ecoli_10_node/Ecoli10-1_dream4_timeseries.tsv"
-gold_standard_file = "data/Ecoli_10_node/Ecoli10-1_goldstandard.tsv"
-df = pd.read_csv(file_path, sep=separator)
 
-# Set SWING parameters
-k_min = 1
-k_max = 3
-w = 10
-method = 'RandomForest'
-trees = 10
 
-# Initialize a SWING object
-sg = Swing(file_path, gene_start_column, gene_end, time_label, separator, min_lag=k_min,
-           max_lag=k_max, window_width=w, window_type=method)
+if __name__ == "__main__":
+    # Load data
+    gene_start_column = 1
+    time_label = "Time"
+    separator = "\t"
+    gene_end = None
+    file_path = "data/Ecoli_10_node/Ecoli10-1_dream4_timeseries.tsv"
+    gold_standard_file = "data/Ecoli_10_node/Ecoli10-1_goldstandard.tsv"
+    df = pd.read_csv(file_path, sep=separator)
 
-sg.zscore_all_data()
-sg.create_windows()
-sg.optimize_params()
+    # Set SWING parameters
+    k_min = 1
+    k_max = 3
+    w = 10
+    method = 'RandomForest'
+    trees = 10
 
-sg.fit_windows(n_trees=trees, show_progress=False, n_jobs=-1)
-sg.rank_edges(permutation_n=1)
-sg.compile_edges(self_edges=False)
+    # Initialize a SWING object
+    sg = Swing(file_path, gene_start_column, gene_end, time_label, separator, min_lag=k_min,
+               max_lag=k_max, window_width=w, window_type=method)
 
-sg.make_static_edge_dict(self_edges=False, lag_method='mean_mean')
-ranked_edges = sg.make_sort_df(sg.edge_dict)
+    sg.zscore_all_data()
+    sg.create_windows()
+    sg.optimize_params()
 
-roc_dict, pr_dict = sg.score(ranked_edges, gold_standard_file=gold_standard_file)
-print(roc_dict['auroc'][-1])
+    sg.fit_windows(n_trees=trees, show_progress=False, n_jobs=-1)
+    sg.rank_edges(permutation_n=1)
+    sg.compile_edges(self_edges=False)
+
+    sg.make_static_edge_dict(self_edges=False, lag_method='mean_mean')
+    ranked_edges = sg.make_sort_df(sg.edge_dict)
+
+    roc_dict, pr_dict = sg.score(ranked_edges, gold_standard_file=gold_standard_file)
+    print(roc_dict['auroc'][-1])
